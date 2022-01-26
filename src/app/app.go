a3dc4fe523de42041298099b5ea488f1a00f47d7
leandrocunha526 <leandrocunha016@gmail.com>
2022-01-26 14:55:41 -0300

    Fix export error to middleware recover, add config and add files

--- src/app/app.go
+++ src/app/app.go
@@ -1,29 +1,29 @@
 package app
 
 import (
-    "github.com/gofiber/fiber"
-    "github.com/gofiber/fiber/middleware/cors"
-    "github.com/gofiber/fiber/middleware/logger"
-    "github.com/gofiber/fiber/middleware/recovery"
-    "github.com/leandrocunha526/moechat/app/user"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/leandrocunha526/moechat/app/user"
 	"github.com/leandrocunha526/moechat/app/user/events/ws"
 )
 
 func Run() {
-  app := fiber.New()
-  db :=DbConn()
-  hub := ws.NewHub()
-  go hub.Run()
-  
-  app.Use(cors.New())
-  app.Use(logger.New())
-  app.Use(recovery.New())
-  
-  user.Init(app, db)
-  
-  app.Get("/", func(c *fiber.Ctx) error {
-      return c.JSON("hello") 
-})
-  
-  app.Listen(":3005")
+	app := fiber.New()
+	db := DbConn()
+	hub := ws.NewHub()
+	go hub.Run()
+
+	app.Use(cors.New())
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	user.Init(app, db)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON("hello")
+	})
+
+	app.Listen(":3005")
 }
