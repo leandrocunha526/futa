625c6235f8e5da5ec8b021bcb22462cfa6685639
Leandro Cunha <leandrocunha016@gmail.com>
2022-05-20 18:08:34 -0300

    Add register package

--- app/user/events/register/repository.go
+++ app/user/events/register/repository.go
@@ -1,1 +1,20 @@
 package register
+
+import (
+	"context"
+	"database/sql"
+	"github.com/leandrocunha526/futa/model/entity"
+)
+
+func Repository(ctx context.Context, tx *sql.Tx, user entity.User) (*entity.User, error) {
+	var lastInsertId int
+	query := "INSERT INTO users(username, passport, email, image) VALUES($1, $2, $3, $4) returning id"
+	err := tx.QueryRowContext(ctx, query, user.Username, user.Password, user.Email, user.Image).Scan(&lastInsertId)
+
+	if err != nil {
+		return &entity.User{}, err
+	}
+
+	user.Id = lastInsertId
+	return &user, nil
+}
